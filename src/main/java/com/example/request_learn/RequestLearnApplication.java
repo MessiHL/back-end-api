908f3131b1db8afa4373da719940a4a8b6782a46
MessiHL <roadsclub@163.com>
2022-04-02 11:33:14 +0800

    feat:新建项目

---
+++ src/main/java/com/example/request_learn/RequestLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.request_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/request_learn/RequestLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.request_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
